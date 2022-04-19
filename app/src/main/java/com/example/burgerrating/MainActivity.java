e3b052cd538c8f43d7edc0b56d23bb125d09344d
Abhishek Soni <yashsoni2826@gmail.com>
2022-04-19 19:12:45 +0530

    Final Commit

---
+++ app/src/main/java/com/example/burgerrating/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.burgerrating;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        textView = findViewById(R.id.textView);
+
+    }
+
+    public void submit(View view) {
+        float ratingvalue = ratingBar.getRating();
+
+        if (ratingvalue < 2){
+            textView.setText("Rating :" + ratingvalue + "\nSorry For InConvinience");
+        }
+        else if (ratingvalue <=3 && ratingvalue>=2){
+            textView.setText("Rating :" + ratingvalue + "\nWe are Improving Day by Day");
+        }
+        else if (ratingvalue <=5 && ratingvalue >3){
+            textView.setText("Rating :" + ratingvalue + "\nThank You For The Rating");
+        }
+
+
+    }
+}
