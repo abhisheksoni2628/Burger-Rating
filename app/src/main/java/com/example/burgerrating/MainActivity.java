6897e5f73230acf1cfe7ad3f3303ed5e80b18a5f
Abhishek Soni <yashsoni2826@gmail.com>
2022-04-21 12:17:44 +0530

    Commit

--- app/src/main/java/com/example/burgerrating/MainActivity.java
+++ app/src/main/java/com/example/burgerrating/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
             textView.setText("Rating :" + ratingvalue + "\nThank You For The Rating");
         }
 
+//        hello guys welcome back
+
 
     }
 }
